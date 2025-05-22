ccdc894aae0f31ce521931e7f522906b4a85b166
sahan yasas maitipe <sahanmaitipe2002@gmail.com>
2025-05-23 00:06:14 +0530

    complete the missing login part in logoutTest

--- src/test/java/edu/sahan/LogoutTest.java
+++ src/test/java/edu/sahan/LogoutTest.java
@@ -1,12 +1,17 @@
 package edu.sahan;
 
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LogoutTest extends AbstractTest {
 
+    @Test
     public void logout(){
 
+        MyTicketsLoginPage loginPage = homePage.goToLoginPage();
+         MyTicketsLoginResults loginResults= loginPage.submitLoginDetails(Secrets.email,Secrets.password);
         MyTicketsLogoutPage logoutPage = homePage.goToLogoutPage();
          MyTicketsLogoutResults logoutResults= logoutPage.submitLogout();
         assertEquals("Sign In",logoutResults.getLogoutResults());
